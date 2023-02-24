762bb8fd522eeeba125e01e5329788d1128c9ff5
Romas <rm@crown-rent.eu>
2023-02-24 15:08:00 +0100

    update commit

--- index.js
+++ index.js
@@ -34,77 +34,74 @@ bot.command('start', async (ctx) => {
     ctx.reply('Bitte Fahrzeugname oder Fahrgestellnummer eingeben:');
   });
 
-bot.on("message", (ctx) => {
+  bot.on("message",(ctx) =>{
 
 
     const { google } = require('googleapis');
     const keys = require('./credentials.json');
-
+    
     // Authenticate with the Google Sheets API using your service credentials
     google.auth.getClient({
-        credentials: keys,
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      credentials: keys,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     }).then(async (client) => {
-
-        // Create an instance of the Google Sheets API
-        const sheets = google.sheets({ version: 'v4', auth: client });
-
-        // Get the data from your Google Sheet
-        const spreadsheetId = '1Dt2OZUrcfErKpIFiRjE2s8_GfMi20LX9CM0-gVjbvgE';
-        const range = 'A:ZZ'; // Replace with your sheet name and range
-        const response = await sheets.spreadsheets.values.get({
-            spreadsheetId,
-            range,
-            
-        });
-        const rows = response.data.values;
-        const array = rows;
-        const firstElement = rows[0];
-        const searchTerm = ctx.message.text;
-
-        // Find the row that contains the search term
-        const result = array.filter(subarray => 
-          subarray.some((item) => 
-            item.toString().toLowerCase().includes(searchTerm.toLowerCase())));
-
-        const secondElement = result;
-        let arr = [];
-
-        for (let j = 0; j < secondElement.length; j++) {
-            const obj = {};
-            for (let i = 0; i < firstElement.length; i++) {
-                obj[firstElement[i]] = secondElement[j][i];
-            }
-            arr.push(obj);
-        }
-
-        for (let k = 0; k < arr.length; k++) { 
-          let replyMessage = `Fahrzeug: ${arr[k].Fahrzeug}\n  
-Dokumente: ${arr[k]['Docs']}\nVIN: ${arr[k].VIN}\nVerkäufer: ${arr[k]['Verkäufer']}\nVon: ${arr[k].Von}\nEinkaufspreis: ${arr[k].Einkaufspreis}\nMwSt: ${arr[k].MwSt}\nLiefertermin: ${arr[k].Liefertermin}\nAnzahlung: ${arr[k]['Anzahlung ']}\nVerkaufspreis: ${arr[k].Verkaufspreis}\nEikaufsdatum: ${arr[k].Eikaufsdatum}\nVertrag: ${arr[k].Vertrag}\nProforma: ${arr[k].Proforma}\nRechnung: ${arr[k].Rechnung}\nDatum: ${arr[k].Datum}\nAn: ${arr[k].An}\nAv: ${arr[k].AV}\nGewinn: ${arr[k].Gewinn}\nKommentare: ${arr[k]["Kommentare"]}\n`;
-
-          // Check each cell of the row for a hyperlink and include it in the reply message if found
-         for (const [key, value] of Object.entries(arr[k])) {
-              if (typeof value === 'string' && value.includes('http')) {
-                  replyMessage += `${key}: <a href="${value}">${value}</a>\n`;
-              }
-          }
-          ctx.replyWithHTML(replyMessage);
-      }
-
-        /*for (let k = 0; k < arr.length; k++) {
-
-            ctx.reply(`Fahrzeug: ${arr[k].Fahrzeug}\n 
-Dokumente: ${arr[k]['Docs']}\nVIN: ${arr[k].VIN}\nVerkäufer: ${arr[k]['Verkäufer']}\nVon: ${arr[k].Von}\nEinkaufspreis: ${arr[k].Einkaufspreis}\nMwSt: ${arr[k].MwSt}\nLiefertermin: ${arr[k].Liefertermin}\nAnzahlung: ${arr[k]['Anzahlung ']}\nVerkaufspreis: ${arr[k].Verkaufspreis}\nEikaufsdatum: ${arr[k].Eikaufsdatum}\nVertrag: ${arr[k].Vertrag}\nProforma: ${arr[k].Proforma}\nRechnung: ${arr[k].Rechnung}\nDatum: ${arr[k].Datum}\nAn: ${arr[k].An}\nAv: ${arr[k].AV}\nGewinn: ${arr[k].Gewinn}\nKommentare: ${arr[k]["Kommentare"]}`);
-        }*/
-
-
-    }).catch((err) => {
-        console.error(err);
+    
+      // Create an instance of the Google Sheets API
+      const sheets = google.sheets({ version: 'v4', auth: client });
+    
+      // Get the data from your Google Sheet
+      const spreadsheetId = '1Dt2OZUrcfErKpIFiRjE2s8_GfMi20LX9CM0-gVjbvgE';
+      const range = 'A1:ZZ'; // Replace with your sheet name and range
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range
+      });
+      
+    const res = await sheets.spreadsheets.get({
+      spreadsheetId,
+      includeGridData: true,
+      ranges: range,
     });
-})
+      const rows = response.data.values;
+    
+    
+      // console.log((rows));
+      const array = rows;
+      const firstElement = rows[0];
+    
+      const searchTerm = ctx.message.text;
+    
+    
+    
+      // Find the row that contains the search term
+      const result = array.filter(subarray => subarray.some(item => item.toString().includes(searchTerm)));
+      const secondElement = result;
+      console.log(result)
+      let arr = [];
+      
+    for(let j = 0; j < secondElement.length; j++){
+      const obj = {};
+      for(let i =0; i < firstElement.length; i++){
+        obj[firstElement[i]] = secondElement[j][i];
+      }
+      arr.push(obj);
+    }
+      for(let k =0; k < arr.length; k++){
+        let replyMessage = `Fahrzeug: ${arr[k].Fahrzeug}\n  
+Dokumente: <a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[1].hyperlink}">Docs</a>\nVIN: ${arr[k].VIN}\nVerkäufer: <a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[3].hyperlink}">${arr[k]['Verkäufer']}</a>\nVon: ${arr[k].Von}\nEinkaufspreis: ${arr[k].Einkaufspreis}\nMwSt: ${arr[k].MwSt}\nLiefertermin: ${arr[k].Liefertermin}\nAnzahlung: ${arr[k]['Anzahlung ']}\nVerkaufspreis: <a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[11].hyperlink}">${arr[k].Verkaufspreis}</a>\nEikaufsdatum: ${arr[k].Eikaufsdatum}\nVertrag:<a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[14].hyperlink}"> ${arr[k].Vertrag}</a>\nProforma: <a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[15].hyperlink}">${arr[k].Proforma}</a>\nRechnung:<a href="${res.data.sheets[0].data[0].rowData[arr[k].No].values[17].hyperlink}"> ${arr[k].Rechnung}</a>\nDatum: ${arr[k].Datum}\nAn: ${arr[k].An}\nAv: ${arr[k].AV}\nGewinn: ${arr[k].Gewinn}\nKommentare: ${arr[k]["Kommentare"]}\n`;
+        
+        ctx.replyWithHTML(replyMessage);
+        console.log(res.data.sheets[0].data[0].rowData[24].values[14])
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
+    })
+    
+    
+    
+    bot.launch();
 
-
-bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
